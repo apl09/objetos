93c1dc04fc17e823b939e8710c50145b24e279a0
apl09 <adrianpastorlopez09@gmail.com>
2023-05-30 17:50:33 +0200

    objetos

---
+++ index.js
@@ -0,0 +1,115 @@
+let Coche = {
+    marca: 'ford',
+    modelo: 'fiesta',
+    matricula: 3456
+
+};
+
+const Casa = {
+    codPostal: 45456,
+    calle: 'Cervantes', 
+    portal: 7,
+    piso: 4
+};
+
+const FullStackDeveloper = {
+      lenguajes: [],
+      proyectos: []
+     
+    
+
+};
+
+const Perro = {
+    nombre: 'Manolo',
+    raza: 'podenco',
+    color: 'marron',
+    edad: 10,
+    ladrar: function() {
+        console.log('guau, guau')
+    },
+    popo: function() {
+        return Math.random()*3
+    }
+
+};
+
+const marcaPortatil = Portatil.marca;
+
+const marcaPortatil2 = Portatil["marca"];
+
+const grupos = Concierto.grupos;
+
+const RGB = Object.values(Led);
+
+ Portatil.modelo = 'P345';
+
+Concierto.cartelera = "Guns N' Roses";
+
+Concierto.fecha = new Date(30/05/23);
+
+Impresora.imprimiendo = Impresora.propiedades;
+
+const Noticia = {
+    titular: 'pepe',
+    cuerpo: 'baila'
+
+};
+
+const Persona = {
+    nombre:'juan',
+    apellidos: 'gracia romero',
+    edad: 98
+};
+
+const Avion = {
+    numPasajeros: 20,
+    despegar: function() {
+        console.log('despegando')
+    },
+    volar: function() {
+        console.log('llegando al destino')
+    },
+    aterrizar: function() {
+        console.log('aterrizando')
+    },
+};
+
+const Paquete = {
+    contenido:[]
+
+};
+
+const Pais = {
+    numHabitantes: 100,
+    continente: 'Europa',
+    gentilicio: 'Huevones'
+};
+
+const codError = O_Error.codigo;
+
+const integrantes = Grupo.integrantes;
+
+const nivelesTinta = Impresora.tinta;
+
+const pixeles = Pantalla.pixeles;
+
+const especificaciones = Movil['especificaciones'];
+
+Grupo.numIntegrantes = 5;
+
+Pantalla.dimensiones = '1920x1080';
+
+Led.encendido = false;
+
+Movil.temperatura = '20º';
+
+
+
+
+    
+
+
+
+
+
